90eb7a5f23d1d17431fdc2acc1df48977428ab54
germancardenasm <germancardenas@gmail.com>
2019-07-10 14:14:57 -0500

    Add a class Note

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,60 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+class Note {
+  constructor(
+    title = "",
+    dateCreated = new Date(),
+    dateModified = new Date(),
+    content = ""
+  ) {
+    this.title = title;
+    this.dateCreated = dateCreated;
+    this.dateModified = dateModified;
+    this.content = content;
+  }
+
+  setContent(note_content) {
+    this.content = note_content;
+  }
+  setTitle(note_title) {
+    this.title = note_title;
+  }
+  setDateCreated(date) {
+    this.dateCreated = date;
+  }
+  setDateModified(date) {
+    this.dateModified = date;
+  }
+
+  get note() {
+    return {
+      title: this.title,
+      dateCreated: this.dateCreated,
+      lastModified: this.dateModified,
+      text: this.text
+    };
+  }
+}
+
+const init = () => {
+  if (!notesAlreadyExist()) {
+    saveInStorage(
+      "notes",
+      new Note(
+        "This is a example note",
+        new Date(),
+        new Date(),
+        `Hi, and thanks for using the new NoteApp`
+      )
+    );
+  }
+  saveInStorage("currentNote", new Note());
+};
+
+const notesAlreadyExist = () => localStorage.getItem("notes");
+
+const saveInStorage = (name, value) =>
+  localStorage.setItem(name, JSON.stringify(value));
+const getFromStorage = name => JSON.parse(localStorage.getItem(name));
+
+init();
